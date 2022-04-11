114b8325aa3d0bd485bc1a6a9145a57c39ae5ab2
shojol <shojolsh@gmail.com>
2022-04-11 22:26:11 +0600

    admin module done

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$conn = mysqli_connect("localhost","root","","library_system") or die("Connection Failed : " . mysqli_connect_error());
+
+$base_url = "Location: http://localhost/project/library-system";
+
+$limit = 8; //limit for paggintion
+?>
